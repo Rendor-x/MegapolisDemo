68808a09f091fe9e8b7e13b047acf075d642e92e
Rendor-x <77492163+Rendor-x@users.noreply.github.com>
2024-02-08 10:16:50 +0300

    Update predprof_demo3.py
    рабочая прога

--- predprof_demo3.py
+++ predprof_demo3.py
@@ -4,8 +4,6 @@ def readFile(filename):
     return list of students
     '''
     f = open(filename, 'r', encoding = 'utf-8')
-    global z1,z2,z3,z4,z5
-    z1,z2,z3,z4,z5=f.readline().strip().split(',')
     tab=[]
     for i in range(500):
         d1,d2,d3,d4,d5=f.readline().strip().split(',')
@@ -16,10 +14,10 @@ def readFile(filename):
 tab = readFile('students.csv')
 idproj=input()
 while idproj!="СТОП":
-    for i in range(len(tab)):
+    for i in range(1, len(tab)):
         fio = tab[i][1].split()
-        if tab[i][4] == idproj:
-            print(f'Проект №{tab[i][4]} делал: {fio[i][0]}.{fio[0]} он(а) получил(а) оценку - {tab[i][6]}.')
+        if tab[i][2] == idproj:
+            print(f'Проект №{tab[i][2]} делал: {fio[1][0]}.{fio[0]} он(а) получил(а) оценку - {tab[i][4]}.')
             break
     else:
         print('Ничего не найдено')
